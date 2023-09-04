8e4c3862d40f8f01acf598d11ac2ae08c62440e4
Giorgia Crosilla <giorgia.crosilla@studio.unibo.it>
2023-09-04 17:58:34 +0200

    update

---
+++ giorgia_html/map.js
@@ -0,0 +1,85 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+
+function ShowMetadata() {
+    const articles = document.querySelectorAll('.testo_metadata');
+    articles.forEach(element => {
+        if (element.id === 'firstarticle') {
+            document.getElementById("s").innerHTML =
+                '<h2>Article Metadata</h2><ul><li>title: The Terror is Born. </li><li>author</li><li>date</li></ul>';
+        }
+    })
+};
+
+function toggleNav() {
+    var sidebar = document.getElementById("mySidebar");
+    var mainContent = document.getElementById("main");
+
+    if (sidebar.style.width === "300px") {
+        sidebar.style.width = "0";
+        mainContent.style.marginLeft = "0";
+    } else {
+        sidebar.style.width = "300px";
+        mainContent.style.marginLeft = "300px";
+    }
+}
+
+function main() {
+    // Checkbox interactions
+    $('#showasides').click(function () {
+        if (this.checked)
+            $('.1700').addClass('n1700');
+        else
+            $('.1700').removeClass('n1700');
+    });
+
+    $('#showspeeches').click(function () {
+        if (this.checked)
+            $('.1800').addClass('n1800');
+        else
+            $('.1800').removeClass('n1800');
+    });
+
+    $('#showquotes').click(function () {
+        if (this.checked)
+            $('.1900').addClass('n1900');
+        else
+            $('.1900').removeClass('n1900');
+    });
+    $('#showpeople').click(function () {
+        if (this.checked)
+            $('.forrest').addClass('nforrest');
+        else
+            $('.forrest').removeClass('nforrest');
+    });
+    $('#showp').click(function () {
+        if (this.checked)
+            $('.brownlow').addClass('nb');
+        else
+            $('.brownlow').removeClass('nb');
+    });
+}
+$(document).ready(main);
