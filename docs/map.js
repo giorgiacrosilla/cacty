42b721a5650a65ece89c9ec5fa3775de3f8c0055
Giorgia Crosilla <giorgia.crosilla@studio.unibo.it>
2023-10-21 17:47:04 +0200

    commenti

--- docs/map.js
+++ docs/map.js
@@ -41,7 +41,8 @@ function changeBackgroundColor() {
     }
 };
 document.addEventListener("DOMContentLoaded", function () {
-    var map = L.map('map').setView([51.505, -0.09], 10);
+    var map = L.map('map').setView([34.225727, -77.944710], 4);
+    console.log(map);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
@@ -72,15 +73,12 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(data => {
                 if (data.features.length > 0) {
                     var coordinates = data.features[0].geometry.coordinates;
-                    var latitude = coordinates[1];
-                    var longitude = coordinates[0];
-                    
-                    // Create a marker for each location and add it to the map
-                    var marker = L.marker([latitude, longitude]).addTo(map)
-                        .bindPopup('<a href="#' + content + '">' + content + '</a>')
-                        .openPopup();
 
-                    // Get a reference to the link and the element with the ID you want to change
+                    // Create a marker for each location
+                    var marker = L.marker([coordinates[1], coordinates[0]]).addTo(map);
+
+                    marker.bindPopup('<a href="#' + content + '">' + content + '</a>');
+
                     var textToChangeColor = document.getElementById(content);
 
                     // Add a click event listener to the marker
@@ -358,36 +356,36 @@ $(document).ready(main);
 
 
 function stylechanger(name) {
-        const buttons = document.querySelectorAll(".style-button");
-        const buttonName = event.currentTarget.getAttribute("name");
-        const styleLink = document.getElementById("style-link");
-        if (styleLink.getAttribute("href") === name) {
+    const buttons = document.querySelectorAll(".style-button");
+    const buttonName = event.currentTarget.getAttribute("name");
+    const styleLink = document.getElementById("style-link");
+    if (styleLink.getAttribute("href") === name) {
         return;
-        }
-        styleLink.setAttribute("href", name);
+    }
+    styleLink.setAttribute("href", name);
 
-        if (name === "De_Stijl.css") {
-            $("#side-image").remove();
-            $("#right-image").remove();
-            $("body").after('<div class="main-container"></div>')
-            $(".main-container").after('<div class="background-grid"></div>')
-            $(".background-grid").after('<div class="block", id="square"></div>')
-            $(".background-grid").after('<div class="block", id="gold-square"></div>')
-            $(".background-grid").after('<div class="block", id="square-3"></div>')
-            $(".background-grid").after('<div class="block", id="yellow-square"></div>')
-            $(".background-grid").after('<div class="block", id="mini-white-square"></div>')
-            $(".background-grid").after('<div class="block", id="blue-square-2"></div>')
-            $(".background-grid").after('<div class="block", id="white-square5"></div>')
-            $(".background-grid").after('<div class="block", id="place-square2"></div>')
-            $(".background-grid").after('<div class="block", id="place-square1"></div>')
+    if (name === "De_Stijl.css") {
+        $("#side-image").remove();
+        $("#right-image").remove();
+        $("body").after('<div class="main-container"></div>')
+        $(".main-container").after('<div class="background-grid"></div>')
+        $(".background-grid").after('<div class="block", id="square"></div>')
+        $(".background-grid").after('<div class="block", id="gold-square"></div>')
+        $(".background-grid").after('<div class="block", id="square-3"></div>')
+        $(".background-grid").after('<div class="block", id="yellow-square"></div>')
+        $(".background-grid").after('<div class="block", id="mini-white-square"></div>')
+        $(".background-grid").after('<div class="block", id="blue-square-2"></div>')
+        $(".background-grid").after('<div class="block", id="white-square5"></div>')
+        $(".background-grid").after('<div class="block", id="place-square2"></div>')
+        $(".background-grid").after('<div class="block", id="place-square1"></div>')
 
-        } else if (name === "Aldine.css") {
-            $("body").before('<img id="right-image" src="https://github.com/giorgiacrosilla/cacty/blob/main/cssfinalimage.png?raw=true")>');
-            $("body").before('<img id="side-image" src="https://github.com/giorgiacrosilla/cacty/blob/main/cssfinalimage.png?raw=true")>');
-            $(".main-container").remove()
-            $(".background-grid").remove()
-            $(".block").remove()
+    } else if (name === "Aldine.css") {
+        $("body").before('<img id="right-image" src="https://github.com/giorgiacrosilla/cacty/blob/main/cssfinalimage.png?raw=true")>');
+        $("body").before('<img id="side-image" src="https://github.com/giorgiacrosilla/cacty/blob/main/cssfinalimage.png?raw=true")>');
+        $(".main-container").remove()
+        $(".background-grid").remove()
+        $(".block").remove()
 
-        }
-    }     
+    }
+}
 
